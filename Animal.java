5074c466fdbd5830f124c3de3d0f442e020d9cbe
rusydajsmn <78582363+rusydajsmn@users.noreply.github.com>
2021-05-03 11:16:01 +0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,7 @@
+package hewan;
+
+public class Animal {
+	public void sound(){
+		System.out.println("Animal is making a soung");
+	}
+}
---
+++ Horse.java
@@ -0,0 +1,14 @@
+package hewan;
+
+public class Horse extends Animal{
+	@Override
+	public void sound(){
+		System.out.println("Neigh");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Animal obj = new Horse();
+		obj.sound();
+	}
+
+}
